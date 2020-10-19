2e3ba34961f92ed33115bda45d42ea59bfc9d10f
TSunT <suntao2020@nuaa.edu.cn>
2020-10-19 23:42:08 +0800

    add Pagehelper base

--- src/main/java/cn/edu/nuaa/myclinic/controller/RouterController.java
+++ src/main/java/cn/edu/nuaa/myclinic/controller/RouterController.java
@@ -19,7 +19,7 @@ public class RouterController {
     public String toLogin(){
         return "login/loginindex";
     }
-    @RequestMapping("/toAdmin")
+//    @RequestMapping("/toAdmin")
     public String toAdmin(){
         return "Admin/Adminindex";
     }
@@ -45,7 +45,7 @@ public class RouterController {
                 String role = objects[0].toString();
                 System.out.println(role);
                 switch (role){
-                    case "admin" : return "forward:/toAdmin";
+                    case "admin" : return "forward:/toAdmin/index";
                     case "doctor" : return "forward:/toDoctor";
                     case "patient" : return "forward:/toPatient";
                 }
--- src/test/java/cn/edu/nuaa/myclinic/MyclinicApplicationTests.java
+++ src/test/java/cn/edu/nuaa/myclinic/MyclinicApplicationTests.java
@@ -1,17 +1,31 @@
 package cn.edu.nuaa.myclinic;
 
+import cn.edu.nuaa.myclinic.pojo.User;
+import cn.edu.nuaa.myclinic.service.AdminService;
 import cn.edu.nuaa.myclinic.service.UserSecurityService;
+
+import com.github.pagehelper.PageInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MyclinicApplicationTests {
     @Autowired
     UserSecurityService userSecurityService;
+    @Autowired
+    AdminService adminService;
     @Test
     void contextLoads() {
         userSecurityService.test();
     }
-
+    @Test
+    void testPage(){
+        List<User> userList = adminService.findAllUser(1, 1);
+        PageInfo pageInfo = new PageInfo(userList);
+        System.out.println(userList);
+        System.out.println(pageInfo);
+    }
 }
