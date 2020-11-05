30ff5d7b2362cada419fd62a7e6bede641a1a90b
TSunT <suntaotao360@live.cn>
2020-11-05 20:08:35 +0800

    fixed time format bug

--- src/main/java/cn/edu/nuaa/myclinic/pojo/Treatmentbrief.java
+++ src/main/java/cn/edu/nuaa/myclinic/pojo/Treatmentbrief.java
@@ -1,5 +1,6 @@
 package cn.edu.nuaa.myclinic.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.Date;
 public class Treatmentbrief implements Serializable {
     private Integer patientid;
     private Integer staffid;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:ss:mm",timezone = "GMT+8")
     private Date time;
     private Integer treatmentid;
     private String diagnose;
