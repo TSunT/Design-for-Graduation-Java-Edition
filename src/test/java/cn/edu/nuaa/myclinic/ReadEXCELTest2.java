484fe4e8e162674d8fde4263f5f1a7800ed9a42b
TSunT <suntaotao360@live.cn>
2021-05-21 15:36:20 +0800

    add poi demo test!.[NOT Completed!]

---
+++ src/test/java/cn/edu/nuaa/myclinic/ReadEXCELTest2.java
@@ -0,0 +1,100 @@
+package cn.edu.nuaa.myclinic;
+
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.util.XMLHelper;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.model.SharedStringsTable;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+public class ReadEXCELTest2 {
+    public void processOneSheet(String filename) throws Exception {
+        OPCPackage pkg = OPCPackage.open(filename);
+        XSSFReader r = new XSSFReader( pkg );
+        SharedStringsTable sst = r.getSharedStringsTable();
+        XMLReader parser = fetchSheetParser(sst);
+        // To look up the Sheet Name / Sheet Order / rID,
+        //  you need to process the core Workbook stream.
+        // Normally it's of the form rId# or rSheet#
+        InputStream sheet2 = r.getSheet("rId1");
+        InputSource sheetSource = new InputSource(sheet2);
+        parser.parse(sheetSource);
+        sheet2.close();
+    }
+    public void processAllSheets(String filename) throws Exception {
+        OPCPackage pkg = OPCPackage.open(filename);
+        XSSFReader r = new XSSFReader( pkg );
+        SharedStringsTable sst = r.getSharedStringsTable();
+        XMLReader parser = fetchSheetParser(sst);
+        Iterator<InputStream> sheets = r.getSheetsData();
+        while(sheets.hasNext()) {
+            System.out.println("Processing new sheet:\n");
+            InputStream sheet = sheets.next();
+            InputSource sheetSource = new InputSource(sheet);
+            parser.parse(sheetSource);
+            sheet.close();
+            System.out.println("");
+        }
+    }
+    public XMLReader fetchSheetParser(SharedStringsTable sst) throws SAXException, ParserConfigurationException {
+        XMLReader parser = XMLHelper.newXMLReader();
+        ContentHandler handler = new SheetHandler(sst);
+        parser.setContentHandler(handler);
+        return parser;
+    }
+    /**
+     * See org.xml.sax.helpers.DefaultHandler javadocs
+     */
+    private static class SheetHandler extends DefaultHandler {
+        private SharedStringsTable sst;
+        private String lastContents;
+        private boolean nextIsString;
+        private SheetHandler(SharedStringsTable sst) {
+            this.sst = sst;
+        }
+        public void startElement(String uri, String localName, String name,
+                                 Attributes attributes) throws SAXException {
+            // c => cell
+            if(name.equals("c")) {
+                // Print the cell reference
+                System.out.print(attributes.getValue("r") + " - ");
+                // Figure out if the value is an index in the SST
+                String cellType = attributes.getValue("t");
+                if(cellType != null && cellType.equals("s")) {
+                    nextIsString = true;
+                } else {
+                    nextIsString = false;
+                }
+            }
+            // Clear contents cache
+            lastContents = "";
+        }
+        public void endElement(String uri, String localName, String name)
+                throws SAXException {
+            // Process the last contents as required.
+            // Do now, as characters() may be called more than once
+            if(nextIsString) {
+                int idx = Integer.parseInt(lastContents);
+                lastContents = sst.getItemAt(idx).getString();
+                nextIsString = false;
+            }
+            // v => contents of a cell
+            // Output after we've seen the string contents
+            if(name.equals("v")) {
+                System.out.println(lastContents);
+            }
+        }
+        public void characters(char[] ch, int start, int length) {
+            lastContents += new String(ch, start, length);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        ReadEXCELTest2 example = new ReadEXCELTest2();
+        example.processOneSheet("D:\\WorkFile\\咪咕相关\\应收应付导入表格专题\\样表\\test1.xlsx");
+        example.processAllSheets("D:\\WorkFile\\咪咕相关\\应收应付导入表格专题\\样表\\test2.xlsx");
+    }
+}
