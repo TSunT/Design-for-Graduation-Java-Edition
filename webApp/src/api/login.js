815bcc5985e9e314aaf10a24490fe7c2be4d4bd5
TSunT <suntao2020@nuaa.edu.cn>
2021-08-19 20:54:02 +0800

    add antd pro vue base project

---
+++ webApp/src/api/login.js
@@ -0,0 +1,84 @@
+import request from '@/utils/request'
+
+const userApi = {
+  Login: '/doLogin',
+  Logout: '/auth/logout',
+  ForgePassword: '/auth/forge-password',
+  Register: '/auth/register',
+  twoStepCode: '/auth/2step-code',
+  SendSms: '/account/sms',
+  SendSmsErr: '/account/sms_err',
+  // get my info
+  UserInfo: '/system/userinfo',
+  UserMenu: '/system/menu/navigate'
+}
+
+/**
+ * login func
+ * parameter: {
+ *     username: '',
+ *     password: '',
+ *     remember_me: true,
+ *     captcha: '12345'
+ * }
+ * @param parameter
+ * @returns {*}
+ */
+export function login (parameter) {
+  console.log({ parameter })
+  return request({
+    url: '/doLogin',
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    data: parameter
+  })
+}
+
+export function getSmsCaptcha (parameter) {
+  return request({
+    url: userApi.SendSms,
+    method: 'post',
+    data: parameter
+  })
+}
+
+export function getInfo () {
+  return request({
+    url: userApi.UserInfo,
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+}
+
+export function getCurrentUserNav () {
+   return request({
+    url: userApi.UserMenu,
+    method: 'get'
+  })
+}
+
+export function logout () {
+  return request({
+    url: userApi.Logout,
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+}
+
+/**
+ * get user 2step code open?
+ * @param parameter {*}
+ */
+export function get2step (parameter) {
+  return request({
+    url: '/doLogin',
+    method: 'post',
+    data: parameter
+  })
+}
